4261d3d234344f8129afe11a039fbbe36f8a7fb6
Vlad <higemaster1@gmail.com>
2023-03-01 10:52:29 +0100

    ngfn

--- src/lesson3/lesson3.clj
+++ src/lesson3/lesson3.clj
@@ -19,3 +19,22 @@
 ;calc factorial and func time in nanosec
 ;WORKING
 
+
+;(defn wedone [i]
+;(defn nanotime [] (. System (nanoTime)))
+;(defn factorial [n]
+;  (if (< n 2) n  (* n (factorial (- n 1)))))
+;(println (factorial i))
+;(def start-time (nanotime))
+;(def end-time (nanotime))
+;(def duration (- end-time start-time))
+;(println duration)
+;)
+;(println (wedone 9))
+
+
+
+;(defn analyze-function [func min max]
+;  (measure-function func min)
+;  (if (< min max)
+;    (analyze-function func (inc min)max)))
